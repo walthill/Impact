ae5d86a4a60939bcd343b48897706d7fa4398157
timferaco <timothy.feraco@mymail.champlain.edu>
2018-04-04 22:06:52 -0400

    Created Branch
    Beginning to dive into Mixer Implementation

--- api/mixerAPI.js
+++ api/mixerAPI.js
@@ -1,20 +1,18 @@
-/* Implementing Mixer API */
-    'use strict';
+'use strict';
 
-    const Mixer = require('beam-client-node');
-    
-    const client = new Mixer.Client(new Mixer.DefaultRequestRunner());
-    
-    client.request('GET', '/channels', {
-    qs: {
-    limit: 5,
-    order: 'viewersCurrent:DESC',
-    },
-    }).then(res => {
-    for (let i = 0; i < res.body.length; i++) {
-    const channel = res.body[i];
-    console.log(channel);
-    console.log(`Your number of viewers is ${channel.viewersCurrent}!`);
-    }
-    });
-    
+const Mixer = require('beam-client-node');
+
+const client = new Mixer.Client(new Mixer.DefaultRequestRunner());
+
+client.request('GET', '/channels', {
+qs: {
+limit: 5,
+order: 'viewersCurrent:DESC',
+},
+}).then(res => {
+for (let i = 0; i < res.body.length; i++) {
+const channel = res.body[i];
+console.log(channel);
+console.log(`Your number of viewers is ${channel.viewersCurrent}!`);
+}
+});
