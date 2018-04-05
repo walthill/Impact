6c4b4c46b87810116d4f2104e8b5cc0444afb5b4
timferaco <timothy.feraco@mymail.champlain.edu>
2018-04-04 22:02:02 -0400

    Creating branch for Mixer implementation and removing all existing code for streams (so I don't break anything)

--- api/mixerAPI.js
+++ api/mixerAPI.js
@@ -1,3 +1,20 @@
-apiKey = "zzww4ur2nncofdcx3eknwqxdm4rwma"; //Registered "Impact" API token
+/* Implementing Mixer API */
+    'use strict';
 
-/*coming soon. My mode of implementation was recently deprecated last year :( */
+    const Mixer = require('beam-client-node');
+    
+    const client = new Mixer.Client(new Mixer.DefaultRequestRunner());
+    
+    client.request('GET', '/channels', {
+    qs: {
+    limit: 5,
+    order: 'viewersCurrent:DESC',
+    },
+    }).then(res => {
+    for (let i = 0; i < res.body.length; i++) {
+    const channel = res.body[i];
+    console.log(channel);
+    console.log(`Your number of viewers is ${channel.viewersCurrent}!`);
+    }
+    });
+    
