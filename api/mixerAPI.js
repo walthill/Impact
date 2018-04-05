072346103aa33f1e9d717cabd08abd2c7ca954a3
timferaco <timothy.feraco@mymail.champlain.edu>
2018-04-05 00:07:59 -0400

    Working on Mixer
    Added basic code for Mixer implementation

--- api/mixerAPI.js
+++ api/mixerAPI.js
@@ -1,18 +1,25 @@
+//WIP=================================================================================
 'use strict';
 
 const Mixer = require('beam-client-node');
 
 const client = new Mixer.Client(new Mixer.DefaultRequestRunner());
 
-client.request('GET', '/channels', {
-qs: {
-limit: 5,
-order: 'viewersCurrent:DESC',
-},
-}).then(res => {
-for (let i = 0; i < res.body.length; i++) {
-const channel = res.body[i];
-console.log(channel);
-console.log(`Your number of viewers is ${channel.viewersCurrent}!`);
+function printStreamsList()
+{
+    client.request('GET', '/channels', {
+    qs: {
+    limit: 5,
+    order: 'viewersCurrent:DESC',
+    },
+    }).then(res => {
+    
+    const feed = document.getElementById("streamsFeed");
+    for (let i = 0; i < 15; i++) {
+        const channel = res.body[i];
+        console.log(channel.id);
+        feed.innerHTML += "<iframe allowfullscreen=\"true\" src=\"https://mixer.com/embed/player/\"" + channel.id + "></iframe>"
+
+    }
+    });
 }
-});
