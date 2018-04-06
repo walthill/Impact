a7342c9ed92f1fe76ec817045f292db13b05a2f9
timferaco <timothy.feraco@mymail.champlain.edu>
2018-04-05 22:01:58 -0400

    Working on Mixer API (WIP)

--- api/mixerAPI.js
+++ api/mixerAPI.js
@@ -16,9 +16,11 @@ function printStreamsList()
     
     const feed = document.getElementById("streamsFeed");
     for (let i = 0; i < 15; i++) {
-        const channel = res.body[i];
+        var channel = res.body[i];
         console.log(channel.id);
-        feed.innerHTML += "<iframe allowfullscreen=\"true\" src=\"https://mixer.com/embed/player/\"" + channel.id + "></iframe>"
+        feed.innerHTML += "<iframe allowfullscreen=\"true\" src=\"https://mixer.com/embed/player/\"" 
+                        + channel.user 
+                        + "></iframe>"
 
     }
     });
