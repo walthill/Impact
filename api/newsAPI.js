60f5f5116fc52817d396f04a5327fe1cb794c38d
Alice Easter <alice.easter@mymail.champlain.edu>
2018-04-05 23:04:03 -0400

    Updated Article Presentation Format (#30)
    * Updated Article Presentation Format
    
    * Further Styling Updates
    
    * Fixed issue where content pane is too short before article load
    
    * Centered Logo

--- api/newsAPI.js
+++ api/newsAPI.js
@@ -12,7 +12,7 @@ const defaultOptions =  'League of Legends OR Super Smash OR Overwatch OR CSGO O
 //POST:     No returns, changes the html of the button to be an iframe of new article
 //PURPOSE:  Launches the desired news article in an iframe within the button that it was pressed
 function displayArticle(newsURL, id) {
-    var newsFeed = document.getElementById(id);  //The button to edit
+    let newsFeed = document.getElementById(id);  //The button to edit
 
     feed.innerHTML = + '<div class="col-10">'
                     + '<iframe src=' + newsURL + '></iframe>'
@@ -73,48 +73,45 @@ function printHeadlines() {
     //News API object
     //The everything keyword queries from everything
     //Can change to 'topHeadlines' for top headlines
-    newsapi.v2.everything({
-        //pageSize: 100,
+    let numArticles = 15;
+    let newsHTML = '';
+
+    newsapi.v2.everything ({
+        pageSize: 99,
         sortBy: 'popularity',
         q: defaultOptions,    //The keywords to search for
         language: 'en'      //The language desired for news
     }).then(response => {
 
-    var text = response;    //Response is what we get back, asssigning it to a variable
-                            //text is a JSON object with an array containing all the news information
-                            //For a breakdown of the message refer to here:
-                            // https://newsapi.org/docs/endpoints/everything
-    //var numResponse = text.totalResults;
+        const feed = document.getElementById("feed");
+        console.log(response);
 
-    const feed = document.getElementById("feed")
+        for (let count = 1; count < numArticles; count++) {
+            //This prints out the news title followed by the description
+            const buttonID = 'button' + count;
+            try
+            {
+                newsHTML +='<div id="' + buttonID + '" class="article bg-secondary">' +
+                    '           <img class="article-thumb" src="' + response.articles[count].urlToImage + '" onerror="SRCcheck(this)">' +
+                    '           <div class="article-details">' +
+                    '               <p class="article-title">' +
+                    '                   <a href="' + response.articles[count].url + '" target="_blank">' +
+                    '                       ' + count + ': ' + response.articles[count].title + '' +
+                    '                   </a>' +
+                    '               </p>' +
+                    '               <p class="article-source">By: ' + response.articles[count].source.name + '</p>' +
+                    '               <p class="article-desc">' + response.articles[count].description + '</p>' +
+                    '               <button class="btn btn-dark" onclick=favoriteArticle("' + response.articles[count].url + '")>Favorite</button>' +
+                    '           </div>' +
+                    '       </div>';
+            }
+            catch (e) {
+                console.log('Error with article: ' + response.articles[count]);
+            }
+        }
+        newsHTML += '<button class="row rounded m-2 p-2 btn" onclick="loadMoreArticles()">More</button>';
 
-    var count = 1;
-    while(count <= 15){
-        //This prints out the news title followed by the description
-        const buttonID = 'button' + count;
-        newsUrl = null;//text.articles[count].url;
-
-        //console.log(buttonID);
-        feed.innerHTML += '<div id=\"' + buttonID + '"\ class=\"row rounded bg-secondary m-2 p-2\">'
-                        + '<div class="col-2">'
-                        + '<img src="'
-						+ text.articles[count].urlToImage
-                        + '" class="articleThumb" onerror="SRCcheck(this)">'
-                        + '</div>'
-                        + '<div class="col-10">'
-                        + "<h2>" + count + ": "
-                        + text.articles[count].title + "</h2>"
-						+ "<p>" + text.articles[count].description
-                        + "</p>"
-                        + "<button class=\"btn btn-dark\" onclick=favoriteArticle(\""
-                        + text.articles[count].url
-                        + "\")>Favorite</button>"
-                        + '</div>'
-                        + '</div>';
-        count++;
-    }
-    feed.innerHTML += '<button class=\"row rounded m-2 p-2 btn\" onclick="loadMoreArticles()">More</button>'
-
+        feed.innerHTML = newsHTML;
   });
 }
 
@@ -124,6 +121,8 @@ function printHeadlines() {
 //          loading more articles until there is none left in the JSON
 //          element that is received
 function loadMoreArticles(){
+
+    let newsHTML = '';
     //Initializes the isMoreArticles boolean if not initialized
     if(typeof loadMoreArticles.isMoreArticles == 'undefined')
         loadMoreArticles.isMoreArticles = true;
@@ -133,7 +132,7 @@ function loadMoreArticles(){
         if(typeof loadMoreArticles.articleCount == 'undefined')
             loadMoreArticles.articleCount = 16;
 
-        startNum = loadMoreArticles.articleCount;
+        let count = loadMoreArticles.articleCount;
         loadMoreArticles.articleCount += 15;
 
         newsapi.v2.everything({
@@ -143,43 +142,45 @@ function loadMoreArticles(){
             language: 'en'        //The language desired for news
         }).then(response => {
 
-            var text = response;
-            console.log(text.totalResults)
-            const feed = document.getElementById("feed")
-            feed.innerHTML = "";
+            const feed = document.getElementById("feed");
 
-            while(startNum <= loadMoreArticles.articleCount && startNum < text.totalResults){
+            if (!response.hasOwnProperty("totalResults"))
+                return;
 
-                if(text[startNum] == 'undefined'){
-                    console.log('caught it')
-                }
+            while(count <= loadMoreArticles.articleCount && count < response.totalResults) {
                 //This prints out the news title followed by the description
                 //console.log(text.articles[count].url);
-                const buttonID = 'button' + startNum;
+                const buttonID = 'button' + count;
                 newsUrl = null;//text.articles[startNum].url;
 
-                feed.innerHTML += '<div id=\"' + buttonID  + '\" class="row rounded bg-secondary m-2 p-2">'
-                                + '<div class="col-2">'
-                                + '<img src="'
-                                + text.articles[startNum].urlToImage
-                                + '" class="articleThumb" onerror="SRCcheck(this)">'
-                                + '</div>'
-                                + '<div class="col-10">'
-                                + "<h2>" + startNum + ": "
-                                + text.articles[startNum].title + "</h2>"
-                                + "<p>" + text.articles[startNum].description
-                                + "</p>"
-                                + "<button class=\"btn btn-dark\" onclick=favoriteArticle(\""
-                                + text.articles[startNum].url
-                                + "\")>Favorite</button>"
-                                + '</div>'
-                                + '</div>'
-                                + '</button>';
-                startNum++;
-                if(startNum == text.totalResults - 1)
+                try
+                {
+                    newsHTML +='<div id="' + buttonID + '" class="article bg-secondary">' +
+                        '           <img class="article-thumb" src="' + response.articles[count].urlToImage + '" onerror="SRCcheck(this)">' +
+                        '           <div class="article-details">' +
+                        '               <p class="article-title">' +
+                        '                   <a href="' + response.articles[count].url + '" target="_blank">' +
+                        '                       ' + count + ': ' + response.articles[count].title + '' +
+                        '                   </a>' +
+                        '               </p>' +
+                        '               <p class="article-source">By: ' + response.articles[count].source.name + '</p>' +
+                        '               <p class="article-desc">' + response.articles[count].description + '</p>' +
+                        '               <button class="btn btn-dark" onclick=favoriteArticle("' + response.articles[count].url + '")>Favorite</button>' +
+                        '           </div>' +
+                        '       </div>';
+                }
+                catch (e) {
+                    console.log('Error with article: ' + response.articles[count]);
+                }
+
+                count++;
+                if(count === response.totalResults - 1)
                     loadMoreArticles.isMoreArticles = false;
             }
-            feed.innerHTML += '<button class=\"row rounded m-2 p-2 btn\" id="loadMoreButton" onclick="loadMoreArticles()">More</button>'
+
+            newsHTML += '<button class=\"row rounded m-2 p-2 btn\" id="loadMoreButton" onclick="loadMoreArticles()">More</button>';
+
+            feed.innerHTML = newsHTML;
 
             document.body.scrollTop = 0;
             document.documentElement.scrollTop = 0;
@@ -188,8 +189,8 @@ function loadMoreArticles(){
     }
     //When there is no more articles to load changed the "More" button to "No more articles to display"
     else{
-        const feed = document.getElementById("loadMoreButton")
-        feed.innerHTML = '<h2> No more articles to display! </h2>'
+        const feed = document.getElementById("loadMoreButton");
+        feed.innerHTML = '<h2> No more articles :(</h2>'
     }
 
 }
@@ -213,17 +214,17 @@ function printUserHeadlines(userOption){
         language: 'en'      //The language desired for news
     }).then(response => {
 
-    var text = response;    //Response is what we get back, asssigning it to a variable
+    let text = response;    //Response is what we get back, asssigning it to a variable
                             //text is a JSON object with an array containing all the news information
                             //For a breakdown of the message refer to here:
                             // https://newsapi.org/docs/endpoints/everything
-    var numResponse = text.totalResults;
+    let numResponse = text.totalResults;
 
     console.log(numResponse);
 
-    const feed = document.getElementById("feed")
+    const feed = document.getElementById("feed");
 
-    var count = 1;
+    let count = 1;
     while(count <= 15){
         //This prints out the news title followed by the description
         feed.innerHTML += '<div class="row rounded bg-secondary m-2 p-2">'
@@ -247,7 +248,7 @@ function printUserHeadlines(userOption){
 
 function favoriteArticle(article) {
 
-    var fs = require('fs')
+    let fs = require('fs');
 
     fs.appendFile('favorites.txt', article + "\n",
         (err) => {
