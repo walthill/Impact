324ccbb3e475c9e928f8b6e4cc7306e5953b7328
tbrabant3 <tyler.brabant@mymail.champlain.edu>
2018-04-18 17:38:31 -0400

    Added functionality to view the favorite articles in an iframe
    Getting errors from them but only due to SSL

--- js/favorites.js
+++ js/favorites.js
@@ -5,15 +5,65 @@ fs.readFile("favorites.txt", "utf8", (err, data) => {
   favs = document.getElementById("favorites")
   var links = data.split(/,| |\n/)
   for(i = 0; i < links.length - 1; i++) {
-	favs.innerHTML += "<li class=\"list-group-item\">"
+	favs.innerHTML += "<li class=\"list-group-item\""
+					+ "id=\"expandView\">"
 					+ "<button class=\"btn btn-outline-danger\" "
 					+ "onClick=unfavorite(\"" + links[i] + "\")>"
 					+ "Unfavorite"
 					+ "</button> "
-					+ links[i]
+					+ "<button class=\"btn btn-outline-succes\" "
+					+ "data-toggle=\"collapse\" data-target=\"#view\""
+					+ "onClick=viewArticle(\"" + links[i] + "\")>"
+					+ "View the article!"
+					+ "</button> "
 					+ "</li>"
   }
 });
+
+function viewArticle(link) {
+	view = document.getElementById("expandView")
+	view.innerHTML = "<button class=\"btn btn-outline-danger\" "
+					+ "onClick=unfavorite(\"" 
+					+ link 
+					+ "\")>"
+					+ "Unfavorite"
+					+ "</button> "
+					+ "<button class=\"btn btn-outline-succes\" "
+					+ "onClick=viewHide(\"" 
+					+ link 
+					+ "\")>"
+					+ "Hide the article!"
+					+ "</button> "
+					+ "<div id=\"view\">"
+					+ "<iframe src=\""
+					+ link
+					+ "\" "
+					+ "width=\""
+					+ 700
+					+ "\" height=\""
+					+ 300
+					+ "\""
+					+ "</iframe>"
+					+ "</div>"
+	
+}
+
+function viewHide(link) {
+	view = document.getElementById("expandView")
+	view.innerHTML = "<button class=\"btn btn-outline-danger\" "
+							+ "onClick=unfavorite(\"" 
+							+ link 
+							+ "\")>"
+							+ "Unfavorite"
+							+ "</button> "
+							+ "<button class=\"btn btn-outline-succes\" "
+							+ "data-toggle=\"collapse\" data-target=\"#view\""
+							+ "onClick=viewArticle(\"" 
+							+ link 
+							+ "\")>"
+							+ "View the article!"
+							+ "</button> "
+}
 
 function unfavorite(link) {
 	fs.readFile("favorites.txt", "utf8", (err, data) => {
